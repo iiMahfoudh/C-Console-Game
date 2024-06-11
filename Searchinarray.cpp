3a7d64a49384c65273766cb24379c81109ad2423
Sellakh Mahfoudh <62813667+iiMahfoudh@users.noreply.github.com>
2024-06-11 21:36:15 +0100

    Update Searchinarray.cpp
    Uppdated by using return property

--- Searchinarray.cpp
+++ Searchinarray.cpp
@@ -8,25 +8,19 @@ int main() {
 	int ent;
 	int num[] = { 10,9,8,7,6,5,4,3,2,1,0 };
 	int size = sizeof(num) / sizeof(int);
-	do {
-		cout << "Entre search : ";
-		cin >> ent;
-		if (ent<0 || ent>size) {
-			cout << "Enter a Valid number !!"<< endl;
-		}
-	} while (ent<0||ent>size);
-	search(ent,num,size);
+	int result;
+	cout << "Entre search : ";
+	cin >> ent;
+	result = search(ent,num,size);
+	cout << "The index value is: " << result << endl;
 	return 0;
 }
 
 int search(int ent, int num[], int size) {
-	cout << "----------------------------------" << endl;
-	cout << "- The Number of total element is :" << size << endl;
-	cout << "----------------------------------" << endl;
 		for (int i = 0; i < size; i++) {
 			if (ent == num[i]) {
-				cout << "The number: " << ent << " is in the row : #" << i << endl;
+				return i;
 			}
 		}
-	return 0;
+		return -1;
 }
