23ff116237ff549c440bde41e1ba4d30787524b4
Sellakh Mahfoudh <62813667+iiMahfoudh@users.noreply.github.com>
2024-06-22 18:14:13 +0100

    Update Function-templates.cpp

--- Function-templates.cpp
+++ Function-templates.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+/* Using Same Function With many argument datatypes and skip overloading */
 template <typename T,typename U>    // Define Datatype created in the Function!
 auto max(T x,U y){  // Set The return type to be auto , Set Argument with the new datatype created!
     return (x>y) ? x : y;   /* Returning max value by ternarry opperator */
