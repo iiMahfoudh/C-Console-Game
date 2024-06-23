0a4f6517bd12e2e05b1e78983cd59ce19a995190
Sellakh Mahfoudh <62813667+iiMahfoudh@users.noreply.github.com>
2024-06-23 19:14:23 +0100

    Update OOP1.cpp

--- OOP1.cpp
+++ OOP1.cpp
@@ -24,9 +24,17 @@ int main(){ //Our Main Function
         iphone.brand = "Iphone X";  // Change This Object Axis
         iphone.model = "X";
         iphone.year = 2019;
+    Phones infinix;
+        infinix.brand = "Infinix inc";
+        infinix.model = "XS";
+        infinix.year = 2017;
     cout << iphone.brand << " " << iphone.model << " " << iphone.year << endl; // Output
     iphone.call();      // Output methode
     iphone.turnon();
     iphone.turnoff();
+    cout << infinix.brand << " " << infinix.model << " " << infinix.year << endl; // Output
+    infinix.call();      // Output methode
+    infinix.turnon();
+    infinix.turnoff();
     return 0;
 }
